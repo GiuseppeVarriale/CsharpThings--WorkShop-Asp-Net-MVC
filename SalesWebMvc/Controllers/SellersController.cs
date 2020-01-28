77ff9d83985b55b02cd1f441aba61b2033c758e9
Giuseppe Varriale <pepe.saoleo@gmail.com>
2020-01-28 20:04:27 -0300

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -47,5 +47,31 @@ namespace SalesWebMvc.Controllers
 
         }
 
+        //Get: Seller/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST: Sellers/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+
+        }
     }
 }
